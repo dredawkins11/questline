40e0e530d19d4156926449773c2ba4457805b2f1
Dre <dredawkins11@gmail.com>
2023-10-24 00:57:32 -0500

    test5

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    console.log(process.env)
     
     return defineConfig({
         plugins: [react()],
         server: {
             proxy: {
                 "/quest": {
-                    target: import.meta.env.VITE_BACKEND_URI,
+                    target: process.env.VITE_BACKEND_URI,
                     changeOrigin: true,
                 },
             },
