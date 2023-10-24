1c91e058574bccd24da45ddf1c321205aef199d2
Dre <dredawkins11@gmail.com>
2023-10-24 00:32:47 -0500

    testy2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default ({ mode }) => {
     console.log(process.env)
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    // process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return defineConfig({
         plugins: [react()],
